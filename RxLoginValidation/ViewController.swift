4e808879b4002565e0b71444dfe457a42686b697
Óscar Rodríguez <oscar.garrucho@gmail.com>
2020-09-08 16:05:57 +0200

    Initial setup with pods

--- RxLoginValidation/ViewController.swift
+++ RxLoginValidation/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var usernameTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var loginButton: UIButton!
+    @IBOutlet private weak var statusLabel: UILabel!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
